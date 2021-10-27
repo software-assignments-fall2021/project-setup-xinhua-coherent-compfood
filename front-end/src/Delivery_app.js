0b9570cb11bd4d3a009ba1e596de6f31fc548153
Andy Huang <huang-a@users.noreply.github.com>
2021-10-26 23:24:48 -0400

    Fix merge conflicts???
    maybe maybe not. this whole thing got confusing fast because reverting and merging a revert will ....... hopefully this works

--- front-end/src/Delivery_app.js
+++ front-end/src/Delivery_app.js
@@ -1,6 +1,7 @@
 import {useState, useEffect} from "react";
 import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
 import "./Delivery_app.css";
+
 let Delivery_app = (props) => {
 	let get_order_id = () => {
 		//TODO actually get the id from the url path
@@ -10,9 +11,9 @@ let Delivery_app = (props) => {
 	return (
 		<div>
 			<p className="p">{props.name}</p>
-			<p className="p">Total: TODO</p>
-			<p className="p">Estimated time to deliver: TODO</p>
-			<a className="go"href={`/processing/${get_order_id()}`}>ORDER</a>
+			<p className="p">Total: {props.price}</p>
+			<p className="p">Estimated time to deliver: {props.time}</p>
+			<a className="go" href={`/processing/${get_order_id()}`}>ORDER</a>
 		</div>
 	);
 };
