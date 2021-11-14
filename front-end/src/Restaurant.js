0eee907705306b6512a965452564c2382eff78f2
Andy Huang <huang-a@users.noreply.github.com>
2021-11-14 15:49:29 -0500

    Change restaurant styling to use flex grid

--- front-end/src/Restaurant.js
+++ front-end/src/Restaurant.js
@@ -12,12 +12,15 @@ let Restaurant = (props) => {
 	return (
 		<div className="boxify">
 			<div className="restaurant-card">
+				<div>
+					<p className="p">{props.name}</p>
+					<a className="go" href={`/restaurant/${generate_order_id()}`}>GO</a>
+					<p className="p">{props.description}</p>
+					<p className="p">Open: {props.hours}</p>
+					<p className="p">Location: {props.location}</p>
+				</div>
+				<div className="flex-divider" />
 				<img src={`${config.backend_base_url}/static/restaurant.jpg`} alt="Random restaurant screen" />
-				<p className="p">{props.name}</p>
-				<a className="go" href={`/restaurant/${generate_order_id()}`}>GO</a>
-				<p className="p">{props.description}</p>
-				<p className="p">Open: {props.hours}</p>
-				<p className="p">Location: {props.location}</p>
 			</div>
 		</div>
 	);
