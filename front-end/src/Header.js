d6a5f6341e336c0aa2c67553851b60d2db0234d7
Phillip Chae <85262429+phillip-chae@users.noreply.github.com>
2021-11-16 13:48:15 -0500

    Update Header.js

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -6,8 +6,8 @@ import "./all.css";
 let Header = (props) => {
 	return (
 		<div className="relative-pos header">
-			<a className="slide-left" href="/"><img src="Home_logo.png" alt="Home logo" width="40" height="50"/></a>
-			<a className="slide-right" href="/previous-orders"><img src="Previous_orders.png" alt="Previous orders logo" width="40" height="50" /></a>
+			<a className="slide-left" href="/"><img src="/Home_logo.png" alt="Home logo" width="40" height="50"/></a>
+			<a className="slide-right" href="/previous-orders"><img src="/Previous_orders.png" alt="Previous orders logo" width="40" height="50" /></a>
 			{//to prevent content overlap due to absolute positioning
 			}
 			<div className="absolute-pos-bar"></div>
