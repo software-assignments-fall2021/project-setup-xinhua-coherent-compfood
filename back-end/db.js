a0fc36c3b72ca9e0c152b45ec07d8ee966096527
Andy Huang <huang-a@users.noreply.github.com>
2021-11-20 15:09:08 -0500

    Fix db code
    login wasn't working and dotenv wasn't imported

--- back-end/db.js
+++ back-end/db.js
@@ -1,15 +1,27 @@
 const mongoose = require("mongoose");
 
+//dotenv loading .env
+require("dotenv").config({
+	silent: true
+});
+
 const User = new mongoose.Schema({
-  first_name: { type: String, default: null },
-  last_name: { type: String, default: null },
-  username: { type: String, unique: true },
-  password: { type: String },
+	first_name: { type: String, default: null },
+	last_name: { type: String, default: null },
+	username: { type: String, unique: true },
+	password: { type: String },
 });
 
- mongoose.connect(process.env.MONGO_DB_URL || 'mongodb://localhost/compfood', {
-       useNewUrlParser: true,
-       useUnifiedTopology: true
-});
+mongoose.connect(
+	process.env.MONGO_DB_URL,
+	{
+		user: process.env.MONGO_DB_USERNAME,
+		pass: process.env.MONGO_DB_PASSWORD,
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	}
+);
 
-module.exports = mongoose.model('User', User);
+module.exports = {
+	User: mongoose.model("User", User)
+};
