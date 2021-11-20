46336fa617cef77e90fe51d205201db261306d8c
Ermias Merine <ebm361@nyu.edu>
2021-11-20 01:24:07 -0500

    Edited app.js

--- back-end/app.js
+++ back-end/app.js
@@ -9,44 +9,42 @@ require("dotenv").config({
  silent: true
 });
  
-require("./config/database").connect();
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
  
 require('./db');
+const User = mongoose.model('User');
+const {verify} = require('./middleware')
 
+const server = express();
+ 
 server.use(bodyParser.urlencoded({extended: true}));
 const cookieParser = require('cookie-parser')
 server.use(cookieParser())
 
-const User = require("./model/user");
-const auth = require("./middleware/auth");
- 
-const server = express();
- 
 server.use(express.json({ limit: "50mb" }));
 
 
-server.post('/register', (req, res) => {
+server.post('/signUp', (req, res) => {
   let flag = 0;
-  
+  console.log('hi')
   const User = mongoose.model('User');
   User.find((err, result) => {
       for(let i = 0; i < result.length; i++){
           if(req.body.username === result[i].username){
               flag = 1;
-      let message = 'Username already exists, please login'
-              res.render('error', {message: message});
-          }
+              let message = 'Username already exists, please login'
+              res.status(200).send(message);
+    }
       } 
       
       if(req.body.password.length < 8){
           if(flag === 0){
               flag = 1;
-      let message = 'Pasword is not long enough, please try again'
-              res.render('error', {message: message});
-          }
+              let message = 'Pasword is not long enough, please try again'
+              res.status(200).send(message);
+    }
       }
 
       if(flag === 0){
@@ -65,17 +63,16 @@ server.post('/register', (req, res) => {
   });
 });
  
- server.post('/login', (req, res) => {
+ server.post('/signIn', (req, res) => {
 	User.findOne({username: req.body.username}, (err, user) => {
         if (!err && user) {
             bcrypt.compare(req.body.password, user.password, (err, passwordMatch) => {
                 if(passwordMatch){
                         if (!err) {
-						curruntuser = req.body.username;
-						let accessToken = jwt.sign({user: user}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "30s"})
-
-						res.cookie("jwt", accessToken, {secure: true, httpOnly: true})
-                        res.redirect('/home');
+						              curruntuser = req.body.username;
+					              	let accessToken = jwt.sign({user: user}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "30s"})
+						              res.cookie("jwt", accessToken, {secure: true, httpOnly: true})
+                          res.redirect('/');
                         } else {
                         console.log('error'); 
                         res.send('an error occurred, please see the server logs for more information');
@@ -83,21 +80,21 @@ server.post('/register', (req, res) => {
                 }
                 else{
                 	let message = 'Incorrect password, please try again'
-
+                  res.status(200).send(message);
                 }
               });
     }
     else{
 		let message = 'Username does not exsist, please register'
-
+    res.status(200).send(message);
     }
     });
 
 });
  
- server.get("/welcome", auth, (req, res) => {
-  res.status(200).send("Welcome 🙌 ");
- });
+ //server.get("/welcome", auth, (req, res) => {
+ // res.status(200).send("Welcome 🙌 ");
+ //});
 
 server.get("/apps", async (req, resp) => {
 	let data = JSON.parse(await fs.readFile("./data/new_apps"));
