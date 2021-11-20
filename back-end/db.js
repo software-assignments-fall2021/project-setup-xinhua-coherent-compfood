0e8e170dc49d046ffc596861b0fb49bd92cd3f77
Ermias Merine <ebm361@nyu.edu>
2021-11-19 22:36:05 -0500

    Created middleware to verify web tokens to protect routes
    Restructured schema
    
    Edited db.js file

--- back-end/db.js
+++ back-end/db.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const User = new mongoose.Schema({
   first_name: { type: String, default: null },
   last_name: { type: String, default: null },
-  email: { type: String, unique: true },
+  username: { type: String, unique: true },
   password: { type: String },
-  token: { type: String },
 });
 
 const MONGODB_URI = 'mongodb+srv://swe474.bimfy.mongodb.net/swe474'
@@ -15,4 +14,5 @@ const MONGODB_URI = 'mongodb+srv://swe474.bimfy.mongodb.net/swe474'
        useUnifiedTopology: true
 });
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model('User', User);
+
