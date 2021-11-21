84c0f8cd1e5f56253e64ec45f0fafd5c718f4da3
Ryeem Zia <ryeemzia@gmail.com>
2021-11-21 18:54:57 -0500

    added restaurant schema

--- back-end/db.js
+++ back-end/db.js
@@ -7,9 +7,16 @@ const User = new mongoose.Schema({
   password: { type: String },
 });
 
+const Restaurant = new mongoose.Schema({
+  name: String,
+  location: String,
+  menu: [String]
+})
+
  mongoose.connect(process.env.MONGO_DB_URL || 'mongodb://localhost/compfood', {
        useNewUrlParser: true,
        useUnifiedTopology: true
 });
 
 module.exports = mongoose.model('User', User);
+module.exports = mongoose.model('Restaurant, Restaurant');
