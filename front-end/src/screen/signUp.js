abce13b2ca4ce716f7c951bad99eda59c766918a
Ermias Merine <ebm361@nyu.edu>
2021-11-23 09:26:19 -0500

    Fixed css path for import

--- front-end/src/screen/signUp.js
+++ front-end/src/screen/signUp.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {useState, useEffect} from "react";
 
 import React from 'react';
-import './signUp.css';
+import '../signUp.css';
 
 const SignUp = () => {
     return (
