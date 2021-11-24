3645dece28cbc36ef6af7367f3b52c6579439324
Andy Huang <huang-a@users.noreply.github.com>
2021-11-23 21:26:33 -0500

    Fix podman usage
    Accidentally used podman instead of docker
    Also had to fix different mounting behavior for volumes in docker vs podman

--- docker_build.sh
+++ docker_build.sh
@@ -3,12 +3,12 @@
 cd front-end
 
 echo " >>> Building frontend <<<"
-podman build -t compfood_frontend -f Dockerfile .
+docker build -t compfood_frontend -f Dockerfile .
 echo " >>> Frontend built <<<"
 
 cd ..
 cd back-end
 
 echo " >>> Building backend <<<"
-podman build -t compfood_backend -f Dockerfile .
+docker build -t compfood_backend -f Dockerfile .
 echo " >>> Backend built <<<"
