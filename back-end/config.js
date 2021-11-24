bc5a1c5248a4020f35743ffec16a7dbdc5dcd974
Andy Huang <huang-a@users.noreply.github.com>
2021-11-23 21:00:34 -0500

    Add docker files for frontend

--- back-end/config.js
+++ back-end/config.js
@@ -8,7 +8,7 @@ let config = {
 	backend_bind_addr: "0.0.0.0",
 	backend_bind_port: 61001,
 
-	backend_base_url: "http://0.0.0.0:61001"
+	backend_base_url: "http://127.0.0.1:61001"
 };
 
 module.exports = config;
