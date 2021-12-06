bd16f1b8fcf6e449731b35ab3afb36560ddfabf7
seocahtoa <seocahtoa@gmail.com>
2021-12-05 23:26:16 -0500

    Styled the order confirmation screen

--- front-end/src/screen/OrderConfirmation.js
+++ front-end/src/screen/OrderConfirmation.js
@@ -1,8 +1,21 @@
+import orderPic from'../orderConfirm.jpg';
 let Order_confirmation = (props) => {
 	return (
 		<div>
-			<p>Order screen</p>
-			<p>TODO TODO TODO</p>
+			<p style={{
+				fontSize: 40,
+				position: 'relative',
+				left: 125
+			}}>Your Order Has Been Placed!</p>
+			<div className="container">
+          	<img src={orderPic} style={{ alignSelf: 'center' }}/>
+        	</div>
+			<p style={{
+				fontSize: 40,
+				position: 'relative',
+				left: 125
+			}}>Please Give us few Moments!
+			</p>
 		</div>
 	);
 };
