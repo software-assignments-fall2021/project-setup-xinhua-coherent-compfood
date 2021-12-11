5a9f08e91ac61dffc7182db4ea477bd7d34ad5f1
Ryeem Zia <ryeemzia@gmail.com>
2021-12-11 12:50:37 -0500

    about page skeleton

--- front-end/src/screen/about.js
+++ front-end/src/screen/about.js
@@ -5,9 +5,19 @@ import '../all.css';
 let About = (props) =>{
     
     return(
-        <div className="divcenter">
-            <p>About</p>
-        </div>
+        <div className="App">
+        <h1>About Us</h1>
+        <div id="image"></div>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget
+            scelerisque neque, quis scelerisque erat. Quisque venenatis molestie
+            sapien, dapibus viverra nulla hendrerit eget. Pellentesque egestas
+            ultrices accumsan. Ut ac magna vel ex maximus ultricies. Nulla facilisi.
+            Suspendisse gravida sem eu odio mattis ullamcorper. Curabitur feugiat
+            ipsum vel vulputate ultricies.
+        </p>
+        
+    </div>
     );
 }
 
