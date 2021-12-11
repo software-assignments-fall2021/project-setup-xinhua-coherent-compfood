aa9807b439f209f7d2ca92b747baec20d3adf543
Ryeem Zia <ryeemzia@gmail.com>
2021-12-11 12:35:17 -0500

    about page screen

---
+++ front-end/src/screen/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import config from "../config";
+import '../all.css';
+
+let About = (props) =>{
+    
+    return(
+        <div className="divcenter">
+            <p>About</p>
+        </div>
+    );
+}
+
+export default About
